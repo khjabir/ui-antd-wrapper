920b90ded483f97db984658ff2eda04c3a521791
Jabir KH <jabir@datalkz.com>
2020-07-15 16:43:06 +0400

    style completely copied version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import sass from 'rollup-plugin-sass'
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from "rollup-plugin-commonjs";
 
 import pkg from './package.json'
 
@@ -18,9 +17,6 @@ export default {
     plugins: [
       sass({ insert: true }),
       typescript({ objectHashIgnoreUnknownHack: true }),
-      commonjs({
-        include: "node_modules/**", // Default: undefined
-      }),
     ],
     external: ['react', 'react-dom', 'antd', 'prop-types']
 }

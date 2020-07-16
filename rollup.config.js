972780e224efdda66dca4bbbd557531f995a1ac0
Jabir KH <jabir@datalkz.com>
2020-07-16 16:31:50 +0400

    :fire: removed unnecessary config files and package json made clean

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,6 @@ import sass from 'rollup-plugin-sass'
 import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
 
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-
 import pkg from './package.json'
 
 export default {
@@ -24,9 +20,6 @@ export default {
       postcss({
         plugins: []
       }),
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
       typescript({ useTsconfigDeclarationDir: true }),
     ],
     external: ['react', 'react-dom', 'antd', 'prop-types', '@ant-design/icons']

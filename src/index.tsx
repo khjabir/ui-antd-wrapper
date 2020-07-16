c06c3022e7ab496158256942695357bb12b7246d
Jabir KH <jabir@datalkz.com>
2020-07-16 13:46:13 +0400

    empty, skeleton and drawer added to npm package

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import './styles/antd.css';
 import DTButton from './DTButton';
 import DTIcon from './DTIcon';
 import DTPageHeader from './DTPageHeader';
+import DTEmpty from './DTEmpty';
+import DTSkeleton from './DTSkeleton';
+import DTDrawer from './DTDrawer';
 
 export {
 	DTButton,
 	DTIcon,
 	DTPageHeader,
+	DTEmpty,
+	DTSkeleton,
+	DTDrawer,
 };

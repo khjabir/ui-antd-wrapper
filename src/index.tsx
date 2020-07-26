d78f470812aa157d5b74334ab8a5f49a06b86c10
Jabir KH <jabir@datalkz.com>
2020-07-26 14:17:24 +0400

    :fire: removed style from source and pointed to node modules folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,9 @@
-import './styles/antd.css';
-import DTButton from './DTButton';
-import DTIcon from './DTIcon';
-import DTPageHeader from './DTPageHeader';
-import DTEmpty from './DTEmpty';
-import DTSkeleton from './DTSkeleton';
-import DTDrawer from './DTDrawer';
+import "../node_modules/antd/dist/antd.css";
+import DTButton from "./DTButton";
+import DTIcon from "./DTIcon";
+import DTPageHeader from "./DTPageHeader";
+import DTEmpty from "./DTEmpty";
+import DTSkeleton from "./DTSkeleton";
+import DTDrawer from "./DTDrawer";
 
-export {
-	DTButton,
-	DTIcon,
-	DTPageHeader,
-	DTEmpty,
-	DTSkeleton,
-	DTDrawer,
-};
+export { DTButton, DTIcon, DTPageHeader, DTEmpty, DTSkeleton, DTDrawer };
